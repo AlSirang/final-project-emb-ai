fb5808a97175386d1caf4f746fd17029d2e27ab3
Bashir Uddin <uddinbashir1999@gmail.com>
2026-02-28 08:58:41 -0500

    format output

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 def emotion_detector(text_to_analyze):
@@ -8,4 +9,20 @@ def emotion_detector(text_to_analyze):
     req_obj= { "raw_document": { "text": text_to_analyze } }
 
     resp = requests.post(url, json=req_obj, headers=headers)
-    return resp.text
+    resp_fmt = json.loads(resp.text)
+    emotions = resp_fmt['emotionPredictions'][0]['emotion']
+    dominant_emotion_score=None
+    dominant_emotion = None
+
+    resp_dict = dict()
+
+    for key,value in emotions.items():
+        resp_dict[key] = value
+        if dominant_emotion_score is None or dominant_emotion_score < value:
+            dominant_emotion_score = value
+            dominant_emotion = key
+    
+    resp_dict['dominant_emotion'] = dominant_emotion
+        
+    
+    return resp_dict
